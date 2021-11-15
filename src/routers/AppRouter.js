7c0528f3bda59fa126133fb42ea15fdc584660af
Luis <ing.luischavezz@gmail.com>
2021-11-14 23:13:48 -0600

    inicio de sesion si está loggeado previamente

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Redirect,
 } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+import { useDispatch } from 'react-redux';
+
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { login } from '../actions/auth';
 
 
 export const AppRouter = () => {
     
-    
+    const dispatch = useDispatch();
+
+    //Iniciar sesión sí ya esta loggeado
+    useEffect( () => {
+        const auth = getAuth();
+
+        onAuthStateChanged( auth, ( user ) => { // "observable" que se dispara cada que el usuario se loggea
+
+            if ( user?.uid ) { // sí 'user' existe y conntiene un 'uid'
+                dispatch( login( user.uid, user.displayName ) );
+            }
+
+        });
+        
+    }, [ dispatch ] );
     
     return (
         <Router>
