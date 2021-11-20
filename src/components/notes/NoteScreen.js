bcd8cb7616e92d81fca0850dffd31be2697b75f3
luischavezz <ing.luischavezz@gmail.com>
2021-11-19 19:31:26 -0600

    Activar las notas al dar click en el journal

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+
 import { NotesAppBar } from './NotesAppBar'
 
 
 export const NoteScreen = () => {
     
-    
+    const { active: note } = useSelector(state => state.notes);
+    console.log(note);
+    // use form
+    const [ formValues, handleInputChange ] = useForm( note );
+    const { title, body } = formValues;
+
+    // console.log(formValues);
     
     return (
         <div className="notes__main-content">
@@ -13,23 +22,34 @@ export const NoteScreen = () => {
             <div className="notes__content">
                 <input 
                     type="text" 
+                    name="title"
                     placeholder="Some awesome title"
                     className="notes__title-input"
                     autoComplete="off"
+                    value={ title }
+                    onChange={ handleInputChange }
                 />
 
-                <textarea 
+                <textarea
+                    name="body"
                     placeholder="What happened today?"
                     className="notes__textarea"
+                    value={ body }
+                    onChange={ handleInputChange }
                 ></textarea>
 
-                <div className="notes__image">
-                    <img 
-                        src="https://pbs.twimg.com/media/Exq5isDW8AIHqzd?format=jpg&name=large"
-                        alt="random_image"
-                    />
-                </div>
-
+                {
+                    ( note.url )
+                        && ( 
+                            <div className="notes__image">
+                                <img 
+                                    src="https://pbs.twimg.com/media/Exq5isDW8AIHqzd?format=jpg&name=large"
+                                    alt="random_image"
+                                />
+                            </div> 
+                        )
+                    
+                }
             </div>
         </div>
     )
